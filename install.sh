24b5ef149a61b62d986a24273d94b38d2de66d5f
dima <zeionara@gmail.com>
2023-01-26 23:54:20 +0300

    Added lua version customization

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 # to install colorful-prompt using this script execute the following command on target machine:
 # curl https://raw.githubusercontent.com/zeionara/colorful-prompt/master/install.sh -s | bash 
 
+lua_version=${LUA_VERSION:-5.4}
+
 quit () {
     echo $1
     exit 1
@@ -11,7 +13,7 @@ quit () {
 install_lua () {
     echo 'installing lua...'
     sudo apt-get update
-    sudo apt-get install lua5.4
+    sudo apt-get install lua$lua_version
 }
 
 lua -v | install_lua
